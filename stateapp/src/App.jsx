5c97245a841d50d0f4dfdf04dc2ea40b89ba0f5d
mayurrajput8486 <mayurrajput8486@gmail.com>
2025-09-26 17:31:09 +0530

    useState()

---
+++ stateapp/src/App.jsx
@@ -0,0 +1,12 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Counter from './components/counter'
+const App = () => {
+  return (
+    <div className='text-primary text-center'>
+      <h1>Learn useState() hook - State in React</h1>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ stateapp/src/components/counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+const counter = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () =>{
+        setCount(count + 1)
+    }
+
+    const decrement = () =>{
+        if(count > 0){
+            setCount(count - 1)
+        }
+        
+    }
+  return (
+    <div className="bg-dark w-50 p-3 rounded-5 mx-auto mt-5">
+        <h2>Counter Application</h2>
+        <div className="btn-group">
+            <button className="btn btn-danger fw-bold fs-3" onClick={increment}>+</button>
+            <button className="btn btn-success fw-bold fs-3">{count}</button>
+            <button className="btn btn-danger fw-bold fs-3" onClick={decrement}>-</button>
+        </div>
+    </div>
+  )
+}
+export default counter
